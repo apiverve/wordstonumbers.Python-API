f2710626304e4b33f9f0d0e0529d6db262ede3fe
APIVerve Team <hello@apiverve.com>
2025-03-28 18:08:43 -0500

    Bug Fixes

--- src/apiverve_wordstonumbers/apiClient.py
+++ src/apiverve_wordstonumbers/apiClient.py
@@ -10,10 +10,10 @@ class WordstonumbersAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/wordstonumbers'
+        self.base_url = 'https://api.apiverve.com/v1/wordstonumbers'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
